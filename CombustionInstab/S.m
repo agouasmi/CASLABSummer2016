c89498096df8c0880e502d13f626fa408ad9a4fa
agouasmi <ayoub.gouasmi@supaero.isae.fr>
2016-05-19 13:12:05 -0400

    Update S.m

--- CombustionInstab/S.m
+++ CombustionInstab/S.m
@@ -1,8 +1,9 @@
 function s = S(x)
+% Computes the localization function ( 1 + sin(\xi) in the paper) for the steady combustion source term
 
 in2m = 0.0254;
 
 ls = 0  * in2m;
-lf = 5 * in2m;
+lf = 5  * in2m;
 
 s = (1 + sin(-pi/2 + 2*pi*(x - ls)/(lf - ls))).*(x < lf).*(x > ls);
