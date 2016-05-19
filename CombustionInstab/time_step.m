a3ee188530131b1c167d749e64863dc3015f8b79
agouasmi <ayoub.gouasmi@supaero.isae.fr>
2016-05-19 13:29:18 -0400

    Update time_step.m

--- CombustionInstab/time_step.m
+++ CombustionInstab/time_step.m
@@ -1,4 +1,5 @@
 function dt = time_step(nc,q)
+% Computes the time-step using a CFL condition
 
 global nu dx
 
@@ -9,7 +10,7 @@ for i = 1:nc
     
       [~,u,~,gam,~,T] = con_to_prim(q(i,:));
       M = MMass(q(i,4)/q(i,1));
-      a = real(sqrt(gam * R/M * T));
+      a = sqrt(gam * R/M * T);
       dtlocal = dx / (abs(u) + a);
       
       dt1 = (dtlocal<dt1)*dtlocal + (dtlocal>=dt1)*dt1;
