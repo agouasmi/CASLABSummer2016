d6142af71f391d2bf0a4af780e1e4fdb9fd9d894
agouasmi <ayoub.gouasmi@supaero.isae.fr>
2016-05-19 13:17:47 -0400

    Update con_to_prim.m

--- CombustionInstab/con_to_prim.m
+++ CombustionInstab/con_to_prim.m
@@ -1,4 +1,5 @@
 function [r,u,p,gam,h,T] =  con_to_prim(q)
+% Converts the conservative variables to primitive variables
 
 run 'constants.m'
 
@@ -6,12 +7,13 @@ Y = q(4)./q(1);
 M = MMass(Y);
 
 gam = gamma_mix(Y);
-% p = (gam-1) * (q(3) - 0.5 * q(2) * q(2) / q(1));
+% p = (gam-1) * (q(3) - 0.5 * q(2) * q(2) / q(1)); This formula, doesn't hold here
 
 r = q(1);
 u = q(2) ./ q(1);
 E = q(3);
 
+% For an explanation of how this is derived see report
 p = (E - 0.5*r.*u.^2 - r.*(Y*dA + A_pr))./(M/R.*(Y*dC + Cp_pr) - 1);
 
 T = p.*M./(r.*R);
